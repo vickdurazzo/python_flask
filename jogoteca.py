ee897588abaf81ac3d68e469458ac9eb36e35c1b
Vick Durazzo <marividurazzo007@gmail.com>
2023-09-12 08:55:06 -0300

    Melhorando o código e a usabilidade
    adicionei o bootstrap e apliquei a prática de reutilização de templates

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -28,6 +28,6 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return render_template('lista.html', titulo='Jogos', jogos=lista)
+    return redirect('/')
 
 app.run(debug=True)
